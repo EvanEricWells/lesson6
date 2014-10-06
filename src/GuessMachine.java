8eb491bef9b69c0e173216b243186d8e657e22a0
evan5090 <evan5090@ISS147D8-19.scdsb.pri>
2014-10-06 09:13:18 -0400

    Initial Commit

---
+++ src/GuessMachine.java
@@ -0,0 +1,14 @@
+
+public class GuessMachine {
+    private int num;
+    private int numguesses;
+    private int guess;
+            
+    public GuessMachine(){
+        num=(int)(Math.random()*100)+1;
+        guess=0;
+        numguesses=0;
+    }
+    //to do - add methods
+    //giveInt(), setGuess(), getNumGuesses()
+}
